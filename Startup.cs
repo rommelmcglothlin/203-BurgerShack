46d83590df569d292a2976e96a2975588c49d610
rommelmcglothlin <rommel.mcglothlin@icloud.com>
2019-10-10 10:06:11 -0600

    updates

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,6 @@ namespace BurgerShack
 
     public IConfiguration Configuration { get; }
 
-<<<<<<< HEAD
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
@@ -36,45 +35,32 @@ namespace BurgerShack
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "BurgerShack", Version = "v1" });
       });
-      services.AddSingleton<FakeDb>();
 
-      services.AddScoped<IDbConnection>(o => CreateDbConnection());
-=======
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSwaggerGen(c =>
+      services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+          .AddCookie(options =>
+          {
+            options.LoginPath = "/Account/Login";
+            //   options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict;
+            //   options.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.SameAsRequest;
+            options.Events.OnRedirectToLogin = (context) =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BurgerShack", Version = "v1" });
-            });
+              context.Response.StatusCode = 401;
+              return Task.CompletedTask;
+            };
+          });
+      services.AddScoped<IDbConnection>(o => CreateDbConnection());
 
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(options =>
-                {
-                    options.LoginPath = "/Account/Login";
-                    //   options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict;
-                    //   options.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.SameAsRequest;
-                    options.Events.OnRedirectToLogin = (context) =>
-                    {
-                        context.Response.StatusCode = 401;
-                        return Task.CompletedTask;
-                    };
-                });
+      services.AddTransient<AccountRepository>();
+      services.AddTransient<AccountService>();
 
-            services.AddScoped<IDbConnection>(o => CreateDbConnection());
+      services.AddTransient<ItemsRepository>();
+      services.AddTransient<ItemsService>();
 
-            services.AddTransient<AccountRepository>();
-            services.AddTransient<AccountService>();
+      services.AddTransient<OrdersRepository>();
+      services.AddTransient<OrdersService>();
 
-            services.AddTransient<ItemsRepository>();
-            services.AddTransient<ItemsService>();
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddTransient<OrdersRepository>();
-            services.AddTransient<OrdersService>();
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-        }
->>>>>>> 617ab6e9eb43efa2092f52584f4aa2681194621c
 
       services.AddTransient<BurgersRepository>();
       services.AddTransient<BurgersService>();
@@ -83,37 +69,8 @@ namespace BurgerShack
       services.AddTransient<OrdersService>();
 
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-    }
 
-<<<<<<< HEAD
-    private IDbConnection CreateDbConnection()
-    {
-      var connectionString = Configuration.GetSection("db").GetValue<string>("gearhost");
-=======
-            app.UseAuthentication();
-
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BurgerShack");
-            });
->>>>>>> 294f4f4416a9cdf12f3a1b136968e45b5cecc48b
-
-      return new MySqlConnection(connectionString);
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
+      app.UseAuthentication();
 
       app.UseSwagger();
       app.UseSwaggerUI(c =>
@@ -121,8 +78,31 @@ namespace BurgerShack
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "BurgerShack");
       });
 
-      app.UseHttpsRedirection();
-      app.UseMvc();
+
+      return new MySqlConnection(connectionString);
     }
+  }
+
+  // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+  public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+  {
+    if (env.IsDevelopment())
+    {
+      app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+      // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+      app.UseHsts();
+    }
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+      c.SwaggerEndpoint("/swagger/v1/swagger.json", "BurgerShack");
+    });
+
+    app.UseHttpsRedirection();
+    app.UseMvc();
   }
 }
